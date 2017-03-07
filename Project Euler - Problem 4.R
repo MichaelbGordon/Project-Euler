0f3508d19187da36682d1d06d6e171049b4d6227
Michael Gordon <michael@me.co.nz>
2017-03-08 10:25:08 +1300

    Problem number 4 complete

---
+++ Project Euler - Problem 4.R
@@ -0,0 +1,31 @@
+
+# Project Euler - Problem 1 -----------
+
+
+# A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+# Find the largest palindrome made from the product of two 3-digit numbers.
+
+
+library(tidyverse)
+
+vector_one <- c(100:999)
+vector_two <- c(100:999)
+
+# Function which returns a TRUE or FLASE if palindrome
+is_palindrome <- function(number) {
+  rawnumber <- charToRaw(number) 
+  identical(rawnumber, rev(rawnumber))
+}
+
+# Produc6ts of all 3 digit number combinations
+df <- data.frame(vector_one,vector_two) %>% 
+      expand(vector_one,vector_two) %>% 
+      mutate(product = vector_one*vector_two)
+
+products <- as.vector(df$product)
+
+# Apply function of all products to check if palindromes
+palindrome <- data.frame(sapply(products, is_palindrome),products)
+
+# Check the largest
+largest_palindrome <- palindrome %>% filter(sapply.products..is_palindrome. == TRUE) %>%  arrange(desc(products))
